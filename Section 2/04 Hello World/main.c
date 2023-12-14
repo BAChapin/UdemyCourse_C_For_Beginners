88ecbceb6ca1f8955c7af94ca68c7a403b2af179
Brett Chapin <brett.chapin89@gmail.com>
2023-12-13 23:55:57 -0500

    Finished lesson 20

---
+++ Section 2/04 Hello World/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World\n");
+    puts("Hello World Again\n");
+    printf("There are %d bottles standing on the %s\n.", 20, "wall");
+    return 0;
+}
