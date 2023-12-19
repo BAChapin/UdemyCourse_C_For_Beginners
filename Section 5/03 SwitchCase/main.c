cead02da71fde698f2a6c01202fde5b2ecaeb121
Brett Chapin <brett.chapin89@gmail.com>
2023-12-18 20:56:24 -0500

    Finished lesson 38

---
+++ Section 5/03 SwitchCase/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+char *chartype;
+
+void findchartype(int i) {
+    switch(i) {
+        case 0:
+            chartype = "NULL";
+            break;
+        case 7:
+            chartype = "Bell";
+            break;
+        case 9:
+            chartype = "Tab";
+            break;
+        case 10:
+            chartype = "LineFeed";
+            break;
+        case 13:
+            chartype = "Carriage Return";
+            break;
+        case 32:
+            chartype = "Space";
+            break;
+        case 78:
+            chartype = "Backspace";
+            break;
+        case 48:
+        case 49:
+        case 50:
+        case 51:
+        case 52:
+        case 53:
+        case 54:
+        case 55:
+        case 56:
+        case 57:
+            chartype = "Number";
+            break;
+        default:
+            chartype = "Character";
+            break;
+    }
+}
+
+void showascii() {
+    int i;
+    char c;
+    for(i = 0; i <= 127; i++) {
+        findchartype(i);
+        printf("%d = %c\t\t[%s]\n", i, i, chartype);
+    }
+}
+
+int main(int argc, char **argv) {
+    showascii();
+    return 0;
+}
